55c6d372bc709a7a820101533db2c5733f1a4ceb
Catherine Kim <catherine@ipwe.com>
2020-08-05 17:41:57 -0700

    Add Binary Search Function Because: - Wrote an iterative implementation of Binary Search
    This commit:
    - Implemented all the methods in the searching file
    - Tested my implementation of the searching methods by running test file.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -15,27 +15,40 @@ def linear_search(arr, target):
 # Driver code
 arr = ['t', 'u', 't', 'o', 'r', 'i', 'a', 'l']
 target = 'r'
-print(f'Element found at index ' + str(linear_search(arr, target)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print(f'Element found at index ' + str(linear_search(arr, target)))
 
 
 # Write an iterative implementation of Binary Search
+# It returns index of target in given array arr if present,
+# else returns -1
 def binary_search(arr, target):
 
-    # Your code here
-
-
+    low = 0
+    high = len(arr) - 1
+    middle = 0
+    
+    while low <= high:
+        middle = (high + low) // 2
+        
+        # check if target is present at middle
+        if arr[middle] < target:
+            low = middle + 1
+            
+        # if target is greater, ignore left half
+        elif arr[middle] > target:
+            high = middle - 1
+            
+        # if target is smaller, ignore right half
+        else:
+            return middle
+        
+    # if we reach here, then the element was not present
     return -1  # not found
+
+# Test array 
+arr = [2, 3, 4, 10, 40]
+target = 10
+
+# Function call
+result = binary_search(arr, target)
+print(f'Element found at index ' + str(binary_search(arr, target)))
